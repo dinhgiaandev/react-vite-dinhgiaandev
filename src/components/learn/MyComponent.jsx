aa2f270a66615cbbb4f6fbde502bed4e760d6d74
dinhgiaandev <dinhgiaanforwork@gmail.com>
2024-06-25 18:03:48 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,10 +2,20 @@
 //JSX : only have 1 parent
 import './style.css'
 const MyComponent = () => {
+   // const dinhgiaandev = "Kato"; // string
+   // const myAge = 20; // number
+   // const testArray = [1, 2, 3] // array
+   const testObject = {
+      name: "Dinhgiaan",
+      age: 20
+   }
    return(
       <>
-         <div> Kato & Dinhgiaandevv</div>
-         <div className='child'> I wanna be a full-stack in 2025</div>
+         <div> {JSON.stringify(testObject)} & Dinhgiaandevv</div>
+         <div> {console.log("Kato")}</div>
+         <div className='child' 
+            style={{fontSize: "3rem"}}
+         > I wanna be a full-stack in 2025</div>
       </>
    );
 }
