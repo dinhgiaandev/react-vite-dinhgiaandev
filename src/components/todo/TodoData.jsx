41532e04f45be5d0ead6cf17528b31ad4876482c
dinhgiaandev <dinhgiaanforwork@gmail.com>
2024-06-25 20:38:43 +0700

    video 34

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,21 @@
+const TodoData = (props) => {
+   //props is a object {}
+   // {
+   //    name: "Dinhgiaannnn",
+   //    age: 21,
+   //    data: {}
+   // }
+   
+   //có thể ghi như này
+   const {name, age, data} = props;
+   console.log(">>> check props: ", props)
+   return (
+      <div className='todo-data'>
+         <div>My name is {name}</div>
+         <div>Learning React</div>
+         <div>Being Full-Stack</div>
+      </div>
+   )
+}
+
+export default TodoData
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,11 @@
+
+const TodoNew = () => {
+   return (
+      <div className='todo-new'>
+         <input type="text" className='input-data' placeholder='Enter your task'/>
+         <button className='button-add'>Add</button>
+      </div>
+   )
+}
+
+export default TodoNew;
