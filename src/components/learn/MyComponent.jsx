46ff189e210635f23f7f3ae4c9965bc367d8692c
dinhgiaandev <dinhgiaanforwork@gmail.com>
2024-06-25 17:26:27 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,12 @@
+// <> : is fragment, which can be help us to have more than 1 div in return
+//JSX : only have 1 parent
+import './style.css'
 const MyComponent = () => {
    return(
-      <div> Kato & Dinhgiaandevv</div>
+      <>
+         <div> Kato & Dinhgiaandevv</div>
+         <div className='child'> I wanna be a full-stack in 2025</div>
+      </>
    );
 }
 
